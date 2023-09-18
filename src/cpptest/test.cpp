61265e7348edb8e62820c7f5ae9c8ec35bc8652f
Johan Larsson <johanlarsson@outlook.com>
2023-09-18 16:06:05 +0200

    fix: use size_t for comparator

--- src/cpptest/test.cpp
+++ src/cpptest/test.cpp
@@ -7,7 +7,7 @@ addVectors(const std::vector<double>& a, const std::vector<double>& b)
 {
   std::vector<double> out;
 
-  for (int i = 0; i < a.size(); ++i) {
+  for (std::size_t i = 0; i < a.size(); ++i) {
     out.emplace_back(a[i] + b[i]);
   }
 
