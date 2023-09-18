a683f248416b79fc232bf9e103799227e6890f51
Johan Larsson <johanlarsson@outlook.com>
2023-09-18 15:42:35 +0200

    chore(build): use lower case for local variables

--- cmake/FindRPackage.cmake
+++ cmake/FindRPackage.cmake
@@ -4,7 +4,7 @@ function(find_r_package module)
   if(NOT R_${module_upper})
     if(ARGC GREATER 1)
       # Not required but we have version constraints.
-      set(VERSION_REQ ${ARGV1})
+      set(version_req ${ARGV1})
     endif()
     # A module's location is usually a directory, but for binary modules it's a
     # .so file.
@@ -20,7 +20,7 @@ function(find_r_package module)
 
     if(NOT _${module}_status)
       # Now we have to check the version.
-      if(VERSION_REQ)
+      if(version_req)
         execute_process(
           COMMAND ${RSCRIPT_EXECUTABLE} "-e" "packageVersion('${module}')"
           RESULT_VARIABLE _version_status
@@ -38,7 +38,7 @@ function(find_r_package module)
         # Compare the version of the package using compareVersion().
         execute_process(
           COMMAND ${RSCRIPT_EXECUTABLE} "-e"
-                  "compareVersion('${_version_compare}', '${VERSION_REQ}')"
+                  "compareVersion('${_version_compare}', '${version_req}')"
           RESULT_VARIABLE _compareVersion_status
           OUTPUT_VARIABLE _compareVersion_result
           OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -49,12 +49,12 @@ function(find_r_package module)
                              "${_compareVersion_result}")
         if("${_compareVersion_result}" GREATER "-1")
           set(R_${module_upper}
-              "${_${module}_location} (found suitable version \"${_version_compare}\", minimum required is \"${VERSION_REQ}\")"
+              "${_${module}_location} (found suitable version \"${_version_compare}\", minimum required is \"${version_req}\")"
               CACHE STRING "Location of R module ${module}")
         else()
           message(
             WARNING
-              "Unsuitable version of R module ${module} (${VERSION_REQ} or greater required)."
+              "Unsuitable version of R module ${module} (${version_req} or greater required)."
           )
         endif()
       else()
